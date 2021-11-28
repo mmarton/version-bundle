b8b833e5640f3b9112cab738d0dbf34afe9e3659
Misley Márton <misley.marton@webdream.hu>
2021-11-28 18:20:46 +0100

    Implement controlelr test

--- Tests/Functional/VersionApiControllerTest.php
+++ Tests/Functional/VersionApiControllerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MauticPlugin\MauticVersionBundle\Tests;
+namespace MauticPlugin\MauticVersionBundle\Tests\Functional;
 
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 
@@ -13,9 +13,10 @@ class VersionApiControllerTest extends MauticMysqlTestCase
         $this->client->request('GET', '/api/version');
         $clientResponse = $this->client->getResponse();
         $clientResponseContent = $clientResponse->getContent();
+        $responseArray = json_decode($clientResponseContent, true, 512, JSON_THROW_ON_ERROR);
 
         self::assertTrue($clientResponse->isOk(), 'Return code must be 200.');
         self::assertResponseHeaderSame('content-type', 'application/json', 'Response content type must be application/json.');
-        self::assertSame(MAUTIC_VERSION, $clientResponseContent, 'Api response must be the actual version.');
+        self::assertSame(MAUTIC_VERSION, $responseArray['version'], 'Api response must be the actual version.');
     }
 }
